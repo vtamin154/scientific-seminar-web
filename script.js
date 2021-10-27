7ac8dabc7f86efda5e9180c7a5aa0e2e485f9ea8
Hoa Nguyen <vtamin154@gmail.com>
2021-10-27 21:49:58 +0700

    featured articles

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if (this.scrollY > 30) {
-        $('.navbar').addClass('sticky');
-      } else {
-        $('.navbar').removeClass('sticky');
-      }
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
+
+  // slide-up script
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
   });
 });
